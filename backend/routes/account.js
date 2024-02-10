634b78a58462d5c4a8e92dc55e4d7442c35d5ee5
imran hassan <hassanimran114@gmail.com>
2024-02-11 00:49:18 +0530

    added tarnsection section

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,11 +1,12 @@
 const express=require('express');
 const authMiddleware=require('../middeware')
-const {Account}=require("../db")
+const {Account}=require("../db");
+const { default: mongoose } = require('mongoose');
 
 const router=express.Router();
 
 
-router.get("/accountCheck", authMiddleware, async (req, res)=>{
+router.get("/balance", authMiddleware, async (req, res)=>{
       const account=await Account.findOne({
             userId:req.userId
       })
@@ -14,6 +15,46 @@ router.get("/accountCheck", authMiddleware, async (req, res)=>{
            balance: account.balance
       })
 })
+
+router.post("/transfer", authMiddleware, async (req, res)=>{
+       const session=await mongoose.startSession()
+       mongoose.startTransaction()
+       const {ammount, to}=req.body;
+       const account=await Account.findOne({userId: req.userId}).session(session);
+       if(!account || account.balance<ammount){
+            await session.abortTransaction();
+            return res.status(400).json({
+                  msg: "invalid account"
+            })
+       }
+
+       await Account.updateOne({
+            userId: req.userId
+       },
+       {
+            $inc: {
+              Balance: -ammount
+            }
+       }
+       ).session(session)
+
+       await Account.updateOne({
+            userId: to
+       },
+       {
+            $inc: {
+                  Balance: ammount
+            }
+       }
+       ).session(session)
+
+       await session.commitTransaction();
+
+       res.json({
+            msg: "Tarnsfer succesfully"
+       });
+
+});
 
 
 module.exports=router
